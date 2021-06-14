8ee74957aae812056b9987359ccd89b595bddb9e
Encho "zolern" Topalov <entopalov@gmail.com>
2021-06-15 02:21:32 +0300

    Initial config

---
+++ user-home-dir/.bash_profile
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# add ~/.bash_profile if needed for executing ~/.bashrc
+
+# alias d='d.cmd' # implemented in its own bash file
+alias df='df.cmd'
+alias ef='ef.cmd'
+alias fe='fe.cmd'
+alias g='g.cmd'
+alias idf='idf.cmd'
+alias kb='kb.cmd'
+alias msb='msb.cmd'
+alias n='n.cmd'
+alias nc='nc.cmd'
+alias nr='nr.cmd'
+alias cls='clear && printf "\e[3J"'
+alias dir='ls -algosAH --group-directories-first'
+
+# delete directory alias
+function rd {
+    if [ -n "$1" ]; then
+        rm -rf "$1"
+    else
+        echo "No dir to delete"
+    fi
+}
+
+# create directory alias
+function md {
+    mkdir "$1"
+}
+
+# delete file alias
+function del {
+    if [ -n "$1" ]; then
+        rm "$1"
+    else
+        echo "No file to delete"
+    fi
+}
+
+# bash wrapper for d.cmd
+function d() {
+	DOUTPUT=$(d.cmd -p "$@")
+	if [[ $? = "99" ]]; then
+		windir=`echo "$DOUTPUT" | sed 's/ *$//g'`
+		linuxdir="$(cygpath -u "$windir")"
+		cd "$linuxdir"
+	else
+		echo "$DOUTPUT"
+	fi
+}
---
+++ user-home-dir/.config/git/git-prompt.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+function __my_windir () {
+  echo "$(cygpath -w "$(pwd)")" # show current dir in Windows path format
+}
+
+PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'\[\033[32m\]'       # change to green
+PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
+PS1="$PS1"'`__my_windir`'    # current working directory (Windows format)
+if test -z "$WINELOADERNOEXEC"
+then
+	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+	COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+	COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+	if test -f "$COMPLETION_PATH/git-prompt.sh"
+	then
+		. "$COMPLETION_PATH/git-completion.bash"
+		. "$COMPLETION_PATH/git-prompt.sh"
+		PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+		PS1="$PS1"'`__git_ps1`'   # bash function
+	fi
+fi
+PS1="$PS1"'\[\033[0m\]'        # change color
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'> '                 # prompt: always $
